46722423486feaf273812d1e0289018f5591c730
Pietari Järvi <pietari.jarvi@gmail.com>
2020-10-02 18:14:09 +0300

    Routing and JSON testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route, Link,
+} from "react-router-dom";
+import Home from "./Components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavItem from "react-bootstrap/NavItem";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="Header">
+                        <Navbar bg="dark" variant="dark">
+                            <Navbar.Brand href="/home" as={Link} to="/home">
+                                WorkoutSwap
+                            </Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <NavItem eventkey={1} href="/Page">
+                                    <Nav.Link as={Link} to="/Page">Page</Nav.Link>
+                                </NavItem>
+                            </Nav>
+                        </Navbar>
+                    </div>
+                    <div className="Main">
+                        <Switch>
+                            <Route path="/home">
+                                <Home />
+                            </Route>
+                            <Route path="/page">
+                                moroo
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
-
-export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {Table} from 'react-bootstrap'
+import axios from 'axios';
+
+export default class Home extends React.Component {
+    state = {
+        notes: [],
+        noteContent: '',
+        noteId: ''
+    };
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/notes')
+            .then(res => {
+                console.log(res);
+                this.setState({notes: res.data})
+            });
+
+    }
+
+    handleChange = event => {
+        this.setState({noteContent: event.target.value});
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const note = {
+            content: this.state.noteContent,
+            id: this.state.noteId
+        };
+
+        axios.post('http://localhost:3001/notes', {note})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+
+            });
+        window.location.reload();
+        return false;
+    };
+
+    render() {
+        return (
+            <div className="Home">
+                <Table striped bordered hover size="sm" variant='dark'>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Content</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.notes.map(note => (
+                        <tr>
+                            <td>{note.id}</td>
+                            <td>{note.content}</td>
+                            <td>{note.date}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </Table>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Content:
+                        <input type='text' name='content' onChange={this.handleChange}/>
+                    </label>
+                    <button type='submit'>Add</button>
+                </form>
+            </div>
+        )
+    }
+}

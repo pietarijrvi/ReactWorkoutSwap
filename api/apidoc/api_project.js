8687822c4f4d4ed4a799449daf42f48faf651e66
jsrti <56799866+jsrti@users.noreply.github.com>
2020-10-15 22:49:28 +0900

    Apidoc files

---
+++ api/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "WorkoutSwap API",
+  "title": "WorkoutSwap API",
+  "url": "http://localhost:9000/",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-15T13:42:57.270Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});

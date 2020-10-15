5a8718c95f123f0425a3371a6122fab58bad44a7
jsrti <56799866+jsrti@users.noreply.github.com>
2020-10-15 20:59:49 +0900

    API comments (apidoc)

--- api/routes/v1/workouts.js
+++ api/routes/v1/workouts.js
@@ -1,28 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const { authJwt } = require("../../middleware");
+const {authJwt} = require("../../middleware");
 const {check, validationResult} = require('express-validator');
 const SqlString = require('sqlstring');
 const connect = require('../../dbConnection.js');
 
-
+/**
+ * @api {get} v1/workouts/ Request all workouts (best rating first)
+ * @apiGroup Workouts
+ *
+ * @apiParam {String} title workout title (or part of it)
+ * @apiParam {Boolean} equipment true:required.
+ * @apiParam {Number} limit result limit.
+ *
+ * @apiSuccess {Number} id id
+ * @apiSuccess {Datetime} createDate  datetime of the workout creation.
+ * @apiSuccess {String} title Workout title.
+ * @apiSuccess {String} description Workout description.
+ * @apiSuccess {Number} duration id
+ * @apiSuccess {Number} equipmentRequired id
+ * @apiSuccess {Number} rating workout popularity (likes)
+ * @apiSuccess {Number} createdBy Id of the user that created the workout
+ *
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+            "id": 18,
+            "createDate": "2020-10-04T15",
+            "title": "testing",
+            "description": "testing",
+            "duration": 3,
+            "equipmentRequired": 0,
+            "rating": 0,
+            "createdBy": 1
+        }
+ */
 router.get('/', function (req, res) {
 
     const limit = req.query.limit;
-    let name = "";
-    if (req.query.title)
-        name = req.query.title;
     const equipment = req.query.equipment;
 
-    //WorkoutId, CreateDate, Title, Description, Duration, EquipmentRequired, Rating, CreatedBy
     let sql;
-    if (req.query.title&&req.query.title!==""){
+    if (req.query.title && req.query.title !== "") {
         sql = SqlString.format("SELECT *"
             + " FROM workouts"
             + " WHERE title LIKE ? AND equipmentRequired = ?"
             + " ORDER BY workouts.rating"
-            + " LIMIT ?", ['%'+req.query.title+'%', equipment, parseInt(limit)]);
-    }else{
+            + " LIMIT ?", ['%' + req.query.title + '%', equipment, parseInt(limit)]);
+    } else {
         sql = SqlString.format("SELECT *"
             + " FROM workouts"
             + " ORDER BY workouts.rating"
@@ -34,6 +59,34 @@ router.get('/', function (req, res) {
 
 });
 
+/**
+ * @api {get} v1/workouts/:id Request all workouts created by a user
+ * @apiGroup Workouts
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {Number} id id
+ * @apiSuccess {Datetime} createDate  datetime of the workout creation.
+ * @apiSuccess {String} title Workout title.
+ * @apiSuccess {String} description Workout description.
+ * @apiSuccess {Number} duration id
+ * @apiSuccess {Number} equipmentRequired id
+ * @apiSuccess {Number} rating workout popularity (likes)
+ * @apiSuccess {Number} createdBy Id of the user that created the workout
+ *
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+            "id": 18,
+            "createDate": "2020-10-04T15",
+            "title": "testing",
+            "description": "testing",
+            "duration": 3,
+            "equipmentRequired": 0,
+            "rating": 0,
+            "createdBy": 1
+        }
+ */
 router.get('/:userId', function (req, res) {
     const sql = SqlString.format("SELECT *"
         + " FROM workouts"
@@ -41,7 +94,18 @@ router.get('/:userId', function (req, res) {
     connect(res, sql);
 });
 
-router.post('/', [authJwt.verifyToken],[
+/**
+ * @api {post} v1/workouts/ Post new workout
+ * @apiGroup Workouts
+ *
+ * @apiParam {Number} title workout title
+ * @apiParam {Number} description workout description
+ * @apiParam {Number} duration workout duration in minutes
+ * @apiParam {Number} equipmentRequired true: equipment required
+ * @apiParam {Number} createdBy userID of the user creating the workout
+ * @apiParam {JSON} JWTokens
+ */
+router.post('/', [authJwt.verifyToken], [
     check('title')
         .isString().withMessage('Only letters and digits allowed in title.')
         .trim()
@@ -55,19 +119,15 @@ router.post('/', [authJwt.verifyToken],[
     // userID must be an int
     check('createdBy').isInt()
 
-    //createDate luodaan sql kanssa
-    //rating=?? votes lähtee nollasta luodaan sql kanssa
 ], function (req, res) {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.log("post err");
         return res.status(422).json({errors: errors.array()});
     }
 
     const rb = req.body;
 
-    console.log("Receiving score - POST");
     const dateTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
     const createdBy = rb.createdBy;
     const rating = 0;

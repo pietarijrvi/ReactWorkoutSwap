a8375c7d65403f20d3acb650faa0125aab1b401f
kimmet <56817045+kimmet@users.noreply.github.com>
2020-10-18 02:27:19 +0300

    1 test added

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
 var assert = require('assert');
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+var expect = chai.expect;
+
+const apiWorkoutsUrl = "http://localhost:9000";
+const note = {
+  description: "this is here for testing purposes",
+  title: "ztest workout",
+  duration: 15,
+  equipmentRequired: false,
+  createdBy: 1
+};
  
 //TESTS HERE
 describe('Array', function () {
-    describe('#indexOf()', function () {
-      it('should return -1 when the value is not present', function () {
-        assert.strictEqual([1, 2, 3].indexOf(4), -1);
+    describe('Adding a workout without user', function () {
+      it('return forbidden: 403', function () {
+        chai.request(apiWorkoutsUrl).post('/api/v1//workouts').send({ note })
+          .then(function(res) {
+            expect(res).to.have.status(403);
+          })
       });
     });
   });
